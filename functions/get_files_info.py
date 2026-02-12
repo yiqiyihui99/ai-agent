646a8457bbce888671a25faac2c51e6b497d9285
yiqiyihui99 <ianwooduffy.99@gmail.com>
2026-02-12 14:33:17 -0800

    refactor: get_files_info and test file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,27 +2,36 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
+    """
+    Gets metadata for current/subdirectory's items, making sure to guard access for out of bounds items/directories
+    """
     if directory == ".":
         header = "Result for current directory:\n"
     else:
         header = f"Result for '{directory}' directory:\n"
     files_info = header
-    new_path = os.path.join(working_directory, directory)  
-    if not os.path.abspath(new_path).startswith(os.path.abspath(working_directory)):
+    combined_path = os.path.join(working_directory, directory)
+    target_dir = os.path.abspath(os.path.normpath(combined_path))
+    working_dir_abs = os.path.abspath(working_directory)  # abs ref req for .commonpath
+    valid_target_dir = (
+        os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    )
+
+    if not valid_target_dir:
         files_info += f'    Error: Cannot list "{directory}" as it is outside the permitted working directory\n'
         return files_info
-    if not os.path.isdir(new_path):
+    if not os.path.isdir(target_dir):
         files_info += f'    Error: "{directory}" is not a directory\n'
         return files_info
     try:
-        for file in os.listdir(new_path):
-                file_path = os.path.join(new_path, file)
-                # output file info
-                name = file,
-                file_size = os.path.getsize(file_path)
-                is_dir = os.path.isdir(file_path)
-                out = f" - {file}: file_size={file_size} bytes, is_dir={is_dir}"
-                files_info += out + "\n"
+        for file in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, file)
+            # output file info
+            name = (file,)
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
+            out = f" - {file}: file_size={file_size} bytes, is_dir={is_dir}"
+            files_info += out + "\n"
     except Exception as e:
         files_info += f"    Error: {e}\n"
     return files_info
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -5,5 +5,7 @@ def test_get_files_info():
     print(get_files_info("calculator", "pkg"))
     print(get_files_info("calculator", "/bin"))
     print(get_files_info("calculator", "../"))
+    print(get_files_info("calculator", "calculator.py"))
     
-test_get_files_info()
+if __name__ == "__main__":
+    test_get_files_info()
