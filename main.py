ff0ebf7d388cc7824c9300c98bbb422a4575a0c7
yiqiyihui99 <ianwooduffy.99@gmail.com>
2026-02-22 10:28:46 -0800

    feat: allow agent to iteratively make calls and  see results

--- main.py
+++ main.py
@@ -24,19 +24,38 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    # Set up the api key and client
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise ValueError("GEMINI_API_KEY is not set")
     client = genai.Client(api_key=api_key)
 
-    return generate_content(client, user_prompt, messages, verbose)
+    for _ in range(20):
+        content, function_responses = generate_content(
+            client, user_prompt, messages, verbose
+        )
+        if content.candidates:
+            for candidate in content.candidates:
+                messages.append(candidate.content)
+        if function_responses:
+            messages.append(types.Content(role="user", parts=function_responses))
+
+        # Early finish should break the loop
+        if not content.function_calls:
+            print("Final response:")
+            print(content.text)
+            break
+
+    if content.function_calls:
+        print(f"No final response reached after 20 iterations")
+        exit(1)
 
 
+# Helper function to generate content using a model and call_function -> should output .parts and .function_response
 def generate_content(client, user_prompt, messages, verbose):
     # Generate content with the client
     model = "gemini-2.5-flash"
+    function_responses = []
 
     try:
         response = client.models.generate_content(
@@ -52,8 +71,8 @@ def generate_content(client, user_prompt, messages, verbose):
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
         if response.function_calls:
+            function_responses = []
             for function_call in response.function_calls:
-                print(f"Calling function: {function_call.name}({function_call.args})")
                 function_call_result = call_function(function_call, verbose=verbose)
                 if not function_call_result.parts:
                     raise Exception("Function call result is empty")
@@ -61,18 +80,16 @@ def generate_content(client, user_prompt, messages, verbose):
                 if first_item.function_response == None:
                     raise Exception("Function response is None")
 
-                function_results = []
-                function_results.append(first_item)
+                function_responses.append(first_item)
                 if verbose:
                     print(
                         f"-> {function_call_result.parts[0].function_response.response}"
                     )
 
-        else:
-            print("Response:")
-            print(response.text)
     except Exception as e:
         print(f"Failed model call on model: {model} with error {e}")
+
+    return response, function_responses
 
 
 if __name__ == "__main__":
